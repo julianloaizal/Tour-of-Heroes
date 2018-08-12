10b6013cf6b0f4dd680af4ce390e3f1f5d69f831
sarra232 <41738408+sarra232@users.noreply.github.com>
2018-08-12 18:18:25 -0500

    Cambio en hero-detail paso 6 del tutorial

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
